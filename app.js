025ecfa72e4b37286176ae6d35192b0faf252648
Martin Angers <martin.n.angers@gmail.com>
2011-04-08 22:09:18 -0400

    Ajoute de mobile.css et du support des media queries

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Just a basic server setup for this site
 var Connect = require('connect');
 
+/*
+ * Resultat de la creation de l'application sur nodester.com:
+ * {"status":"success","port":9346,
+ *  "gitrepo":"ec2-user@nodester.com:/node/hosted_apps/puerkitobio/1193-c0eb17a63c110c2c8128089d77ff255a.git",
+ *  "start":"app.js","running":false,"pid":"unknown"}
+*/
 module.exports = Connect.createServer(
   Connect.logger(),
   //Connect.conditionalGet(),
@@ -8,5 +14,5 @@ module.exports = Connect.createServer(
   //Connect.cache(),
   //Connect.gzip(),
   require('wheat')(__dirname)
-).listen(3000);
+).listen(9346);
 
